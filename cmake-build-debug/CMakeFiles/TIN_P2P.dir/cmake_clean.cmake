402aee6c0191c1593a6a2575b09ce8cb2b83cc2e
Hubert Kuczyński <bertas55@gmail.com>
2017-01-01 22:36:23 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/TIN_P2P.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TIN_P2P.dir/main.cpp.o"
+  "TIN_P2P.pdb"
+  "TIN_P2P"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TIN_P2P.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
