4080f3a9416a104e1001759f5a2831d79d89a7c6
Lukasz <dziwiedz@gmail.com>
2017-01-05 17:59:21 +0100

    Still not completed yet

--- SocketCreator.cpp
+++ SocketCreator.cpp
@@ -3,3 +3,74 @@
 //
 
 #include "SocketCreator.h"
+#include <sys/socket.h>
+
+using namespace std;
+
+Socket* SocketCreator::CreateSocket(void)
+{
+    int descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if(descriptor < 0)
+    {
+        return nullptr;
+    }
+    return new Socket(descriptor);
+}
+
+Socket* SocketCreator::CreateSocket(const string &address, unsigned short port, bool connect)
+{
+    Socket* s = CreateSocket();
+    if(s == nullptr)
+    {
+        return nullptr;
+    }
+    if(connect)
+    {
+        if(s->Connect(address, port) == false)
+        {
+            delete s;
+            s = nullptr;
+        }
+    }
+    else
+    {
+        if(s->Bind(address, port) == false)
+        {
+            delete s;
+            s = nullptr;
+        }
+    }
+    return s;
+
+}
+
+Socket* SocketCreator::CreateServerSocket(void)
+{
+    int descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if(descriptor < 0)
+    {
+        return nullptr;
+    }
+    return new ServerSocket(descriptor);
+}
+
+Socket* SocketCreator::CreateServerSocket(unsigned short port)
+{
+    Socket* s = CreateServerSocket();
+    if(s == nullptr)
+    {
+        return nullptr;
+    }
+    if(s->Bind(port) == false)
+    {
+        delete s;
+        s = nullptr;
+        return nullptr;
+    }
+    if(s->Listen() == false)
+    {
+        delete s;
+        s = nullptr;
+    }
+    return s;
+}
--- SocketCreator.h
+++ SocketCreator.h
@@ -4,10 +4,19 @@
 
 #ifndef TIN_P2P_SOCKETCREATOR_H
 #define TIN_P2P_SOCKETCREATOR_H
-
+#include "Socket.h"
 
 class SocketCreator {
+public:
+    static Socket* CreateSocket(void);
+    static Socket* CreateSocket(const std::string& address, unsigned short port, bool connect);
+    static Socket* CreateServerSocket(void);
+    static Socket* CreateServerSocket(unsigned short port);
 
+private:
+    SocketFactory(void);
+    SocketFactory(const SocketFactory&);
+    SocketFactory& operator=(const SocketFactory&);
 };
 
 
